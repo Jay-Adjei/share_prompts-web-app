097f9783fdc73af55ab297ae2d79148e0f35b14f
Jay-Adjei <jphtadjei@gmail.com>
2024-05-07 14:08:34 +0000

    Feed Page

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+'use client'
+import { useState, useEffect } from "react"
+
+import PromptCard from "./PromptCard"
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt-layout">
+      {/* Iterates through the prompts in the data prop and renders the promptcard component for each post. */}
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  const handleSearchChange = (e) => {
+
+  }
+
+  useEffect (() => {
+    const fetchPosts = async() => {
+      //Gets all prompts from the database
+      const response = await fetch('/api/prompt');
+      //Converts the response to JSON
+      const data = await response.json();
+      //Sets the posts state to the data from the database
+      setPosts(data);
+    }
+    //Calls the fetchPosts function when the component mounts
+    fetchPosts();
+  }, [])
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input 
+          type="text"
+          placeholder="Search for a tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList
+      //Passes the posts state to the PromptCardList component
+      data={posts}
+      handleTagClick={() => {}}
+      
+    />
+    </section>
   )
 }
 
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import { useState } from "react"
+import Image from "next/image"
+import { useSession } from "next-auth/react"
+import { usePathname, useRouter } from "next/navigation"
 
-const PromptCard = () => {
+const PromptCard = ({ post, handleTagClick}) => {
+  const [copied, setCopied] = useState('')
+
   return (
-    <div>PromptCard</div>
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+          <Image 
+          src={post.creator.image}
+          alt="User_image"
+          width={40}
+          height={40}
+          className="rounded-full object-contain"
+        />
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {post.creator.username}
+            </h3>
+            <p className="font-inter text-sm text-gray-500">
+              {post.creator.email}
+            </p>
+          </div>
+        </div>
+          {/*Copy button */}
+        <div className="copy_btn" onClick={() => {}}>
+          {/* checks is the prompt copied is equal to the prompt intended to copy and renders an image based on that */}
+          <Image src={copied === post.prompt?
+            'assets/icons/tick.svg'
+            : '/assets/icons/copy.svg'
+            }
+            width={12}
+            height={12}
+          />
+        </div>
+      </div>
+
+      <p className="my-4 font-satoshi text-sm text-gray-700">
+        {post.prompt}
+        </p>
+      <p className="font-inter text-sm blue_gradient cursor-pointer"
+      onClick={() => handleTagClick && handleTagClick(post.tag)}> {/*If tag exists then we will be able to click and show all relevant/similar tags */}
+        {post.tag}
+      </p>
+    </div>
   )
 }
 
