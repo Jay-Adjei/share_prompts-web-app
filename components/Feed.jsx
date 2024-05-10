773f70516438cc4a68db107f8834f4e7f346bdd5
Jay-Adjei <jphtadjei@gmail.com>
2024-05-10 14:15:48 +0000

    Search bar functionality

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -19,12 +19,11 @@ const PromptCardList = ({ data, handleTagClick }) => {
 }
 
 const Feed = () => {
+
   const [searchText, setSearchText] = useState('');
+  const [searchTimeOut, setSearchTimeOut] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
   const [posts, setPosts] = useState([]);
-
-  const handleSearchChange = (e) => {
-
-  }
 
   useEffect (() => {
     const fetchPosts = async() => {
@@ -39,6 +38,44 @@ const Feed = () => {
     fetchPosts();
   }, [])
 
+  const filterPosts = (searchtext) => {
+    const searchPattern = new RegExp(searchtext, 'i');
+    return posts.filter(
+      (item) => 
+        searchPattern.test(item.creator.username) ||
+        searchPattern.test(item.tag) ||
+        searchPattern.test(item.prompt)
+    )
+  }
+
+  const handleSearchChange = (e) => {
+    //clear any lingering timeout before setting a new one
+    clearTimeout(searchTimeOut);
+    //set the search text to the value of the user input
+    setSearchText(e.target.value);
+
+    //set a new timeout to search for the text after 700ms
+    setSearchTimeOut(
+      //call the filterPosts function with the search text
+    setTimeout(() => {
+      //filter the posts based on the search text
+      const searchResult = filterPosts(e.target.value);
+      //set the searched results state to the search result
+      setSearchedResults(searchResult);
+    }), 1000)
+
+  }
+  const  handleTagClick = (tag) => {
+    //set the search text to the tag in turn changing the value of the search input to the tag clicked
+    setSearchText(tag);
+
+    //filter the posts based on the tag clicked
+    const tagResult = filterPosts(tag);
+    //set the searchedResult state to the tagResult
+    setSearchedResults(tagResult);
+  }
+
+
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
@@ -52,11 +89,26 @@ const Feed = () => {
         />
       </form>
 
-      <PromptCardList
-        //Passes the posts state to the PromptCardList component
-        data={posts}
-        handleTagClick={() => {}}
-      />
+      {/* Condition to check if the user has typed in anything 
+      in the search bar and render the PromptCardList component 
+      with the searchedResults state */}
+      {
+        searchText? (
+          <PromptCardList
+          //Passes the searchedResults state to the PromptCardList component
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+          
+        ) : (
+          <PromptCardList
+          //Passes the posts state to the PromptCardList component
+          data={posts}
+          handleTagClick={handleTagClick}
+        />
+        )
+      }
+
     </section>
   )
 }
