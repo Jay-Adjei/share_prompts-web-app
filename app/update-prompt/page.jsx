5fe11c8c524b19eea3a9f99318933aa00ebadf68
Japhet Adofo-Adjei <jphtadjei@gmail.com>
2024-07-09 18:24:31 +0000

    Update page.jsx

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from "react"
+import { useEffect, useState, Suspense } from "react"
 import { useRouter, useSearchParams} from "next/navigation"
 
 import Form from "@components/Form"
@@ -59,13 +59,15 @@ const EditPrompt = () => {
     }
 
   return (
-    <Form
-    type='Edit'
-    post={post}
-    submitting={submitting}
-    setPost={setPost}
-    handleSubmit={updatePrompt}
-    />
+    <Suspense fallback={<div>Loading...</div>}>
+            <Form
+                type='Edit'
+                post={post}
+                submitting={submitting}
+                setPost={setPost}
+                handleSubmit={updatePrompt}
+            />
+        </Suspense>
   );
 }
 
