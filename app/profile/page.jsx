3322c5dcd143ff584b138b44c32debd141195acb
Japhet Adjei <mr.adjei17@gmail.com>
2024-07-30 03:33:27 +0000

    logIn fix

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -25,32 +25,33 @@ const MyProfile = () => {
 
   const handleEdit = (post) => {
     //Navigate to the update prompt page with the id of the post (adds a query parameter named id to the URL)
-    router.push(`/update-prompt?id=${post._id}`)
+    router.push(`/update-prompt?id=${post._id}`);
   };
 
   const handleDelete = async (post) => {
-    const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
 
-    if(hasConfirmed){
+    if (hasConfirmed) {
       try {
-        await fetch(`/api/prompt/${post._id.toString()}`,{
-          method: 'DELETE',
-  
-        } )
-          //if the current element's(p) _id is different from the one being deleted(post.id), include it in the new filteredPosts array , otherwise exclude it.
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+        //if the current element's(p) _id is different from the one being deleted(post.id), include it in the new filteredPosts array , otherwise exclude it.
         const filteredPosts = posts.filter((item) => item._id !== post._id);
 
-        setPosts(filteredPosts)
+        setPosts(filteredPosts);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     }
   };
 
   return (
     <Profile
-      name='My'
-      desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination'
+      name="My"
+      desc="Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
       data={posts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
